aafa285106ee381df651d3a6cce58610fafc7a64
김부추 <qkqdlek32@gmail.com>
2023-07-02 00:16:43 +0900

    @Builder 제거

--- src/main/java/com/example/demo/presentation/exception/UrlExceptionResponse.java
+++ src/main/java/com/example/demo/presentation/exception/UrlExceptionResponse.java
@@ -1,12 +1,10 @@
 package com.example.demo.presentation.exception;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-@Builder
 public class UrlExceptionResponse {
     private String message;
 }
