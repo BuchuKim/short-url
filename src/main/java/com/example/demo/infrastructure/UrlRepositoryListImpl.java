faa560d5836fd5bc65f60296103ebe0505b383b7
김부추 <qkqdlek32@gmail.com>
2023-07-03 22:54:27 +0900

    hashmap 구현체 추가

--- src/main/java/com/example/demo/infrastructure/UrlRepositoryListImpl.java
+++ src/main/java/com/example/demo/infrastructure/UrlRepositoryListImpl.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class UrlRepositoryImpl implements UrlRepository {
+public class UrlRepositoryListImpl implements UrlRepository {
     private final List<ShortenUrl> shortenUrls = new ArrayList<>();
 
     @Override
---
+++ src/main/java/com/example/demo/infrastructure/UrlRepositoryMapImpl.java
@@ -0,0 +1,49 @@
+package com.example.demo.infrastructure;
+
+import com.example.demo.domain.ShortenUrl;
+import com.example.demo.domain.UrlNotFoundException;
+import com.example.demo.domain.UrlRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+public class UrlRepositoryMapImpl implements UrlRepository {
+    private final Map<String, String> urls = new HashMap<>();
+
+    @Override
+    public List<ShortenUrl> findAll() {
+        return urls.keySet().stream()
+                .map(key ->
+                        ShortenUrl.builder()
+                                .shortenUrl(key)
+                                .originalUrl(urls.get(key))
+                                .build())
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public ShortenUrl findByShortenUrl(String shortenUrl) {
+        String originalUrl = urls.get(shortenUrl);
+        if (originalUrl==null) throw new UrlNotFoundException();
+
+        return ShortenUrl.builder()
+                .originalUrl(originalUrl)
+                .shortenUrl(shortenUrl)
+                .build();
+    }
+
+    @Override
+    public ShortenUrl save(ShortenUrl shortenUrl) {
+        urls.put(shortenUrl.getShortenUrl(),shortenUrl.getOriginalUrl());
+        return shortenUrl;
+    }
+
+    @Override
+    public int getTotalUrlSize() {
+        return urls.size();
+    }
+}
