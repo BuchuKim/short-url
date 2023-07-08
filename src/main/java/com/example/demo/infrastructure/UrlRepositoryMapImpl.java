49997f6caa71f6c6aa1d4cbb3c056d2433838dc4
김부추 <qkqdlek32@gmail.com>
2023-07-08 13:13:35 +0900

    map value 수정

--- src/main/java/com/example/demo/infrastructure/UrlRepositoryMapImpl.java
+++ src/main/java/com/example/demo/infrastructure/UrlRepositoryMapImpl.java
@@ -5,44 +5,26 @@ import com.example.demo.domain.UrlRepository;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.*;
 
 @Repository
 @Profile("map")
 public class UrlRepositoryMapImpl implements UrlRepository {
-    private final Map<String, String> urls = new HashMap<>();
+    private final Map<String, ShortenUrl> urls = new HashMap<>();
 
     @Override
     public List<ShortenUrl> findAll() {
-        return urls.keySet().stream()
-                .map(key ->
-                        ShortenUrl.builder()
-                                .shortenUrl(key)
-                                .originalUrl(urls.get(key))
-                                .build())
-                .collect(Collectors.toList());
+        return (List<ShortenUrl>) urls.values();
     }
 
     @Override
     public Optional<ShortenUrl> findByShortenUrl(String shortenUrl) {
-        String originalUrl = urls.get(shortenUrl);
-        if (originalUrl==null) {
-            return Optional.empty();
-        } else {
-            return Optional.of(ShortenUrl.builder()
-                    .originalUrl(originalUrl)
-                    .shortenUrl(shortenUrl)
-                    .build());
-        }
+        return Optional.of(urls.get(shortenUrl));
     }
 
     @Override
     public ShortenUrl save(ShortenUrl shortenUrl) {
-        urls.put(shortenUrl.getShortenUrl(),shortenUrl.getOriginalUrl());
+        urls.put(shortenUrl.getShortenUrl(), shortenUrl);
         return shortenUrl;
     }
 }
